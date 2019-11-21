b6aaef78cf32553642d96dac624594acb65abc50
Programmer <amir_k3085@yahoo.com>
2019-11-21 20:27:24 +0330

    Added lazyloading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,9 @@ import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
 
+
+
+
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
